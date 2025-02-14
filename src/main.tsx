07d2714f161852e72decd2ee7b064bb0a3887629
vcidst <hello@shailendra.me>
2025-02-14 22:47:24 +0100

    add trailing slash for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/rasa-playground">
+    <BrowserRouter basename="/rasa-playground/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
