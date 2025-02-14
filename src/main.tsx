b35639e6d6944f0c74a3e0b7baeafe5aabded391
vcidst <hello@shailendra.me>
2025-02-14 22:36:42 +0100

    add 404 page and handle github pages base

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/rasa-playground">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
